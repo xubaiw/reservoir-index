801a042c2d010eaa3078f36b21fed34b17013e59
xubaiw <xubaiw@users.noreply.github.com>
2022-03-06 06:17:56 +0000

    Deploying to gh-pages from @ xubaiw/reservoir-index@e3f68601a9abc50450389f9ff12699fd9ca6ff4a 🚀

--- git/leanprover/lean4/RELEASES.md
+++ git/leanprover/lean4/RELEASES.md
@@ -115,3 +115,25 @@ def Array.insertAtAux (i : Nat) (as : Array α) (j : Nat) : Array α :=
 ```
 
 * Add support for `for h : x in xs do ...` notation where `h : x ∈ xs`. This is mainly useful for showing termination.
+
+* Auto implicit behavior changed for inductive families. An auto implicit argument occurring in inductive family index is also treated as an index. For example
+```lean
+inductive HasType : Index n → Vector Ty n → Ty → Type where
+```
+is now interpreted as
+```lean
+inductive HasType : {n : Nat} → Index n → Vector Ty n → Ty → Type where
+```
+
+* Added auto implicit "chaining". Unassigned metavariables occurring in the auto implicit types now become new auto implicit locals. Consider the following example:
+```lean
+inductive HasType : Fin n → Vector Ty n → Ty → Type where
+  | stop : HasType 0 (ty :: ctx) ty
+  | pop  : HasType k ctx ty → HasType k.succ (u :: ctx) ty
+```
+`ctx` is an auto implicit local in the two constructors, and it has type `ctx : Vector Ty ?m`. Without auto implicit "chaining", the metavariable `?m` will remain unassigned. The new feature creates yet another implicit local `n : Nat` and assigns `n` to `?m`. So, the declaration above is shorthand for
+```lean
+inductive HasType : {n : Nat} → Fin n → Vector Ty n → Ty → Type where
+  | stop : {ty : Ty} → {n : Nat} → {ctx : Vector Ty n} → HasType 0 (ty :: ctx) ty
+  | pop  : {n : Nat} → {k : Fin n} → {ctx : Vector Ty n} → {ty : Ty} → HasType k ctx ty → HasType k.succ (u :: ctx) ty
+```
---
+++ git/leanprover/lean4/tests/lean/run/interp.lean
@@ -0,0 +1,62 @@
+inductive Vector (α : Type u) : Nat → Type u
+  | nil : Vector α 0
+  | cons : α → Vector α n → Vector α (n+1)
+
+infix:67 (priority := high) " :: " => Vector.cons
+
+inductive Ty where
+  | int
+  | bool
+  | fn (a r : Ty)
+
+@[reducible] def Ty.interp : Ty → Type
+  | int    => Int
+  | bool   => Bool
+  | fn a r => a.interp → r.interp
+
+inductive HasType : Fin n → Vector Ty n → Ty → Type where
+  | stop : HasType 0 (ty :: ctx) ty
+  | pop  : HasType k ctx ty → HasType k.succ (u :: ctx) ty
+
+open HasType (stop pop)
+
+inductive Expr : Vector Ty n → Ty → Type where
+  | var : HasType i ctx ty → Expr ctx ty
+  | val : Int → Expr ctx Ty.int
+  | lam : Expr (a :: ctx) ty → Expr ctx (Ty.fn a ty)
+  | app : Expr ctx (Ty.fn a ty) → Expr ctx a → Expr ctx ty
+  | op  : (a.interp → b.interp → c.interp) → Expr ctx a → Expr ctx b → Expr ctx c
+  | ife : Expr ctx Ty.bool → (Unit → Expr ctx a) → (Unit → Expr ctx a) → Expr ctx a
+
+inductive Env : Vector Ty n → Type where
+  | nil  : Env Vector.nil
+  | cons : Ty.interp a → Env ctx → Env (a :: ctx)
+
+def Env.lookup : HasType i ctx ty → Env ctx → ty.interp
+  | stop,  Env.cons x xs => x
+  | pop k, Env.cons x xs => lookup k xs
+
+def Expr.interp (env : Env ctx) : Expr ctx ty → ty.interp
+  | var i     => env.lookup i
+  | val x     => x
+  | lam b     => fun x => b.interp (Env.cons x env)
+  | app f a   => f.interp env (a.interp env)
+  | op o x y  => o (x.interp env) (y.interp env)
+  | ife c t e => if c.interp env then (t ()).interp env else (e ()).interp env
+
+open Expr
+
+/- Examples -/
+
+def add : Expr ctx (Ty.fn Ty.int (Ty.fn Ty.int Ty.int)) :=
+  lam (lam (op (.+.) (var stop) (var (pop stop))))
+
+#eval add.interp Env.nil 10 20
+
+def fact : Expr ctx (Ty.fn Ty.int Ty.int) :=
+  lam (ife (op (.==.) (var stop) (val 0))
+           (fun _ => val 1)
+           (fun _ => op (.*.) (app fact (op (.-.) (var stop) (val 1))) (var stop)))
+  decreasing_by sorry
+
+#eval fact.interp Env.nil 10
