3bdda2c2382e531d2e0687234eb5d53b0593ab3c
xubaiw <xubaiw@users.noreply.github.com>
2022-03-15 06:17:24 +0000

    Deploying to gh-pages from @ xubaiw/reservoir-index@19d9d83e5c6f230ba4d0b32c0d5abff1a3ebeeed 🚀

--- git/crabbo-rave/lean4-Tuple/Examples.lean
+++ git/crabbo-rave/lean4-Tuple/Examples.lean
@@ -47,4 +47,7 @@ def HListPatternMatch (l: HList [Nat, String]) :=
 #eval toString [1, "2", 3.0, [4, 5, 6], #[7, 8]]
 #eval toString HList.nil
 
-#eval 1 ::: ["1", 5.4, '4']
+def baz : HList [Nat, String, Float, Char] := 
+  1 :: ["1", 5.4, '4']
+
+#eval baz
