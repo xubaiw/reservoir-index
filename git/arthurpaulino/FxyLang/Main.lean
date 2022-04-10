2b3ce9c9da9f5236feddbb2e6dd2918710145c80
xubaiw <781449311@qq.com>
2022-04-10 14:33:30 +0000

    Deploying to gh-pages from @ xubaiw/reservoir-index@1a0f5ccc03ab002a7874319bcd297e8caf79affa 🚀

---
+++ git/arthurpaulino/FxyLang/Main.lean
@@ -0,0 +1,19 @@
+import Lean
+import FxyLang
+
+open Lean
+
+def main : List String → IO Unit
+  | [f] => do
+    let code ← IO.FS.readFile ⟨f⟩
+    initSearchPath (← Lean.findSysroot) ["build/lib"]
+    let env ← importModules [{ module := `FxyLang.Parser }] {}
+    match ← parse code env with
+    | (none    , p) =>
+      let (c, r) ← p.runIO
+      IO.println r
+      IO.println c
+    | (some msg, _) => IO.eprintln msg
+  | _   => do
+    let appName := (← IO.appPath).fileName.getD "extern"
+    IO.eprintln s!"Usage: {appName} <fxy-file>"
---
+++ git/arthurpaulino/FxyLang/lakefile.lean
@@ -0,0 +1,6 @@
+import Lake
+open Lake DSL
+
+package FxyLang {
+  supportInterpreter := true
+}
