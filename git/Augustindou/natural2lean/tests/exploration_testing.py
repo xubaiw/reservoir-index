9ab7253390b755719b66fa6fc2d4957bb0e99c61
xubaiw <xubaiw@users.noreply.github.com>
2022-04-18 06:19:33 +0000

    Deploying to gh-pages from @ xubaiw/reservoir-index@822bf1e9e88ca7e25fa58ff6f37f017026f9aaca 🚀

---
+++ git/Augustindou/natural2lean/tests/exploration_testing.py
@@ -0,0 +1,8 @@
+from natural2lean.structure.theorem import Theorem
+from natural2lean.text.multiple_propositions import MultiplePropositions
+
+if __name__ == "__main__":
+    example = "$a^2$ is divisible by 3"
+
+    object = MultiplePropositions(example)
+    print(object.translate())
