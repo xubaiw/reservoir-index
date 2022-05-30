2793b3faa5f51ccbfbc86fff7d7b8685e2ddd9d4
xubaiw <781449311@qq.com>
2022-05-30 06:15:54 +0000

    Deploying to gh-pages from @ xubaiw/reservoir-index@5e075386241dbdee7f4a4721c9d130b10754e71c 🚀

--- git/siddhartha-gadgil/Polylean/Polylean/UnitConjecture.lean
+++ git/siddhartha-gadgil/Polylean/Polylean/UnitConjecture.lean
@@ -18,8 +18,31 @@ abbrev x : P := (P.x, P.e)
 abbrev y : P := (P.y, P.e)
 abbrev z : P := (P.z, P.e)
 abbrev a : P := ((0, 0, 0), P.a)
-abbrev b : P := ((0, 0, 1), P.b)
+abbrev b : P := ((0, 0, 0), P.b)
+abbrev one : RP := 1
+
+#eval (one + x⁻¹) * (one + y) = one + x⁻¹ + y + x⁻¹ * y
 
 def eg : RP := x * x + y⁻¹ * b
 
 #eval eg * eg
+
+def p : RP := one +  x +  y +  x*y +  z⁻¹ + x*z⁻¹ + y*z⁻¹ + x*y*z⁻¹
+def q : RP := (x⁻¹*y⁻¹ : RP) + x + y⁻¹*z + z
+def r: RP := one + x + y⁻¹*z + x*y*z
+def s : RP  := one + x*z⁻¹ + x⁻¹*z⁻¹ + y*z⁻¹ + y⁻¹*z⁻¹
+
+def α := p + (q * a) + (r * b) + (s * a * b)
+ 
+def p' : RP := x⁻¹ * (a⁻¹  * p * a)
+def q' : RP := -(x⁻¹ * q)
+def r' : RP := -(y⁻¹ * r)
+def s' : RP := z⁻¹ * (a⁻¹ * s * a)
+
+def α' := p' + (q' * a) + (r' * b) + (s' * a * b)
+
+def unitsProd := α' * α 
+
+theorem units : unitsProd = 1 := by native_decide
+
+theorem non_trivial : α ≠ 1 := by native_decide
