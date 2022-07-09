089133f613bea96f332ed6ed629bd4cf1ff415ef
xubaiw <xubaiw@users.noreply.github.com>
2022-07-09 18:17:52 +0000

    Deploying to gh-pages from @ xubaiw/reservoir-index@e79c7221f4adc572173fa2d42261274198517387 🚀

--- git/JamesGallicchio/LeanColls/LeanColls/FoldableOps.lean
+++ git/JamesGallicchio/LeanColls/LeanColls/FoldableOps.lean
@@ -8,25 +8,29 @@ import LeanColls.Classes
 
 namespace LeanColls
 
-class FoldableOps (C τ) [Foldable C τ] [BEq τ] where
+class FoldableOps (C τ) [Foldable C τ] where
   toList : C → List τ
-  h_toList : toList = Foldable.fold (· :: ·) []
   all : C → (τ → Bool) → Bool
-  h_all : all = List.all ∘ toList
-  contains : C → τ → Bool
-  h_contains : contains = List.contains ∘ toList
-
-instance [Foldable C τ] [BEq τ] : Inhabited (FoldableOps C τ) where
-  default := {
-    toList := _
-    h_toList := rfl
-    all := _
-    h_all := rfl
-    contains := _
-    h_contains := rfl
-  }
+  contains : C → [BEq τ] → τ → Bool
+  sum : C → [Add τ] → [OfNat τ 0] → τ
 
 namespace FoldableOps
+
+def defaultImpl (F : Foldable C τ) : FoldableOps C τ where
+  toList    := λ c      => Foldable.fold (fun acc x => x::acc) [] c |>.reverse
+  all       := λ c f    => Foldable.fold (fun acc x => acc && f x) true c
+  contains  := λ c _ x  => Foldable.fold (fun acc y => acc || BEq.beq x y) true c
+  sum       := λ c      => Foldable.fold (fun acc x => acc + x) 0 c
+
+instance [F : Foldable C τ] : Inhabited (FoldableOps C τ) where
+  default := defaultImpl F
+
+/- TODO: figure out how to generate foldable' generically
+def foldable' [F : Foldable C τ] [B : BEq τ] [M : Membership τ C]
+  (h_mem : ∀ c x, x ∈ c ↔ (defaultImpl F B).contains c x)
+  : Foldable' C τ M where
+  fold' := F.fold
+  -/
 
 theorem toList_ind [Foldable C τ] [BEq τ] [FoldableOps C τ] {α : Type u} {motive : List τ → Sort v}
         (nil : motive List.nil)
