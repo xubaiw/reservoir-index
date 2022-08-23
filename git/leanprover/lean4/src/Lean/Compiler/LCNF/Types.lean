dc2e4d41828eff7c82db899c59f1a9f8bfffeae0
xubaiw <xubaiw@users.noreply.github.com>
2022-08-23 06:38:48 +0000

    Deploying to gh-pages from @ xubaiw/reservoir-index@b54219959d48841f3189ae9babce08780b5a27e4 🚀

--- git/leanprover/lean4/src/Lean/Compiler/LCNF/Types.lean
+++ git/leanprover/lean4/src/Lean/Compiler/LCNF/Types.lean
@@ -5,9 +5,8 @@ Authors: Leonardo de Moura
 -/
 import Lean.Compiler.BorrowedAnnotation
 import Lean.Meta.InferType
-import Lean.Compiler.LCNF.Types
 
-namespace Lean.Compiler
+namespace Lean.Compiler.LCNF
 
 structure LCNFTypeExtState where
   types : Std.PHashMap Name Expr := {}
@@ -19,6 +18,12 @@ builtin_initialize lcnfTypeExt : EnvExtension LCNFTypeExtState ←
 
 def erasedExpr := mkConst ``lcErased
 def anyTypeExpr := mkConst  ``lcAny
+
+def _root_.Lean.Expr.isAnyType (e : Expr) :=
+  e.isConstOf ``lcAny
+
+def _root_.Lean.Expr.erased (e : Expr) :=
+  e.isConstOf ``lcErased
 
 /-!
 The code generator uses a format based on A-normal form.
@@ -178,6 +183,41 @@ partial def compatibleTypes (a b : Expr) : Bool :=
       | .const n us, .const m vs => n == m && List.isEqv us vs Level.isEquiv
       | _, _ => false
 
+mutual
+
+partial def joinTypes (a b : Expr) : Expr :=
+  joinTypes? a b |>.getD anyTypeExpr
+
+partial def joinTypes? (a b : Expr) : Option Expr := do
+  if a.isAnyType then return a
+  else if b.isAnyType then return b
+  else if a == b then return a
+  else if a.erased || b.erased then failure
+  else
+    let a' := a.headBeta
+    let b' := b.headBeta
+    if a != a' || b != b' then
+      joinTypes? a' b'
+    else
+      match a, b with
+      | .mdata _ a, b => joinTypes? a b
+      | a, .mdata _ b => joinTypes? a b
+      | .app f a, .app g b =>
+        (do return .app (← joinTypes? f g) (← joinTypes? a b))
+         <|>
+        return anyTypeExpr
+      | .forallE n d₁ b₁ _, .forallE _ d₂ b₂ _ =>
+        (do return .forallE n (← joinTypes? d₁ d₂) (joinTypes b₁ b₂) .default)
+        <|>
+        return anyTypeExpr
+      | .lam n d₁ b₁ _, .lam _ d₂ b₂ _ =>
+        (do return .lam n (← joinTypes? d₁ d₂) (joinTypes b₁ b₂) .default)
+        <|>
+        return anyTypeExpr
+      | _, _ => return anyTypeExpr
+
+end
+
 /--
 Return `true` if `type` is a LCNF type former type.
 -/
@@ -197,4 +237,4 @@ def isClass? (type : Expr) : CoreM (Option Name) := do
   else
     return none
 
-end Lean.Compiler
+end Lean.Compiler.LCNF
